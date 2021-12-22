8adb396e5719a8cbfa564354ecf160edf18a6126
neuromeow <92692649+neuromeow@users.noreply.github.com>
2021-12-22 19:54:04 +0300

    Refactor connection configuration handlers

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,22 +1,11 @@
 from aiogram import Dispatcher, types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import CommandStart, Text
-from aiogram.dispatcher.filters.state import State, StatesGroup
+
 from aiogram.types.message import ContentType
 
-from bot.keyboards import start_menu_keyboard, generate_connection_keyboard
-from bot.misc import IsAdmin
-
-
-class ConnectionStatus(StatesGroup):
-    configuration = State()
-
-
-class ConfigurationOptions(StatesGroup):
-    hostname = State()
-    port = State()
-    user = State()
-    password = State()
+from bot.keyboards import generate_configuration_menu_keyboard, start_menu_keyboard
+from bot.misc import ConnectionStatus, ConfigurationOptions, IsAdmin
 
 
 async def cmd_start(message: types.Message, state: FSMContext):
@@ -28,18 +17,20 @@ async def cmd_start(message: types.Message, state: FSMContext):
     )
 
 
-async def generate_connection_menu(message: types.Message, state: FSMContext):
+async def generate_configuration_menu(message: types.Message, state: FSMContext):
     configuration = await state.get_data()
     await message.answer(
-        "\n".join(f"{option}: {value}" for option, value in configuration.items())
-        if configuration else "Connect button will appear after all settings have been added...",
-        reply_markup=generate_connection_keyboard(**configuration)
+        "\n".join(f"{option.title()}: {value}" for option, value in configuration.items())
+        if configuration
+        else "Configure the options.\n"
+             "The connect button will be available after configuring all.",
+        reply_markup=generate_configuration_menu_keyboard(**configuration)
     )
     await ConnectionStatus.configuration.set()
 
 
-async def configure_connection_button(callback: types.CallbackQuery, state: FSMContext):
-    await generate_connection_menu(callback.message, state)
+async def configuration_menu_button(callback: types.CallbackQuery, state: FSMContext):
+    await generate_configuration_menu(callback.message, state)
     await callback.answer()
 
 
@@ -58,18 +49,23 @@ async def enter_option_value(message: types.Message, state: FSMContext):
 
 
 async def update_option_value(option: str, message: types.Message, state: FSMContext) -> None:
+    """Sets and updates the value of the passed option."""
     if len(message.text) == 4096:
         await message.reply(f"Please note that this message will be added as {option} value.")
     configuration = await state.get_data()
     configuration[option] = message.text
     await state.update_data(configuration)
-    await generate_connection_menu(message, state)
+    await generate_configuration_menu(message, state)
 
 
-async def reset_configuration(callback: types.CallbackQuery, state: FSMContext):
+async def reset_button(callback: types.CallbackQuery, state: FSMContext):
     await state.reset_data()
     await callback.message.edit_text("The current values of the connection configuration options have been reset.")
-    await generate_connection_menu(callback.message, state)
+    await generate_configuration_menu(callback.message, state)
+
+
+async def connect_button(callback: types.CallbackQuery, state: FSMContext):
+    pass
 
 
 async def undefined_request(message: types.Message):
@@ -80,9 +76,10 @@ async def undefined_request(message: types.Message):
 
 def register_handlers(dp: Dispatcher):
     dp.register_message_handler(cmd_start, IsAdmin(), CommandStart(), state="*")
-    dp.register_message_handler(generate_connection_menu, IsAdmin(), commands=["connection"], state="*")
-    dp.register_callback_query_handler(configure_connection_button, Text(equals="connection"))
+    dp.register_message_handler(generate_configuration_menu, IsAdmin(), commands=["connect"], state="*")
+    dp.register_callback_query_handler(configuration_menu_button, Text(equals="configure"))
     dp.register_callback_query_handler(option_button, Text(endswith="option"), state=ConnectionStatus.configuration)
     dp.register_message_handler(enter_option_value, state=ConfigurationOptions.states_names)
-    dp.register_callback_query_handler(reset_configuration, Text(equals="reset"), state=ConnectionStatus.configuration)
+    dp.register_callback_query_handler(reset_button, Text(equals="reset"), state=ConnectionStatus.configuration)
+    dp.register_callback_query_handler(connect_button, Text(equals="connect"), state=ConnectionStatus.configuration)
     dp.register_message_handler(undefined_request, content_types=ContentType.ANY, state="*")
--- bot/keyboards.py
+++ bot/keyboards.py
@@ -1,26 +1,25 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-def generate_connection_keyboard(
+def generate_configuration_menu_keyboard(
         hostname: str = None, user: str = None, password: str = None, port: str = None
 ) -> InlineKeyboardMarkup:
-    connection_keyboard = InlineKeyboardMarkup(
-        inline_keyboard=[
-            [InlineKeyboardButton(f"{'✔' if hostname else '➕'} HostName", callback_data="hostname_option")],
-            [InlineKeyboardButton(f"{'✔' if port else '➕'} Port", callback_data="port_option")],
-            [InlineKeyboardButton(f"{'✔' if user else '➕'} User", callback_data="user_option")],
-            [InlineKeyboardButton(f"{'✔' if password else '➕'} Password", callback_data="password_option")]
-        ]
+    configuration = locals()
+    options_buttons = [
+        InlineKeyboardButton(f"{'Edit' if value else '➕ Add'} {option}", callback_data=f"{option}_option")
+        for option, value in configuration.items()
+    ]
+    configuration_keyboard = InlineKeyboardMarkup(row_width=1).add(*options_buttons)
+    if any(configuration.values()):
+        configuration_keyboard.add(InlineKeyboardButton("➰ Reset", callback_data="reset"))
+    configuration_keyboard.add(
+        InlineKeyboardButton(f"{'👨🏻‍💻' if all(configuration.values()) else '🚫'} Connect", callback_data="connect")
     )
-    if hostname or user or password or port:
-        connection_keyboard.add(InlineKeyboardButton("➰ Reset", callback_data="reset"))
-    if hostname and user and password and port:
-        connection_keyboard.add(InlineKeyboardButton("👨🏻‍💻 Connect", callback_data="connect"))
-    return connection_keyboard
+    return configuration_keyboard
 
 
 start_menu_keyboard = InlineKeyboardMarkup(
     inline_keyboard=[
-        [InlineKeyboardButton("⚙ Connect via SSH", callback_data="connection")]
+        [InlineKeyboardButton("⚙ Connect via SSH", callback_data="configure")]
     ]
 )
