47af5e8e5e899cbd12040457e77828827fbfc882
neuromeow <92692649+neuromeow@users.noreply.github.com>
2021-12-22 17:23:54 +0300

    Add and set up the ability to add configuration options

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,10 +1,22 @@
 from aiogram import Dispatcher, types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import CommandStart, Text
+from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types.message import ContentType
 
 from bot.keyboards import start_menu_keyboard, generate_connection_keyboard
 from bot.misc import IsAdmin
+
+
+class ConnectionStatus(StatesGroup):
+    configuration = State()
+
+
+class ConfigurationOptions(StatesGroup):
+    hostname = State()
+    port = State()
+    user = State()
+    password = State()
 
 
 async def cmd_start(message: types.Message, state: FSMContext):
@@ -17,23 +29,41 @@ async def cmd_start(message: types.Message, state: FSMContext):
 
 
 async def generate_connection_menu(message: types.Message, state: FSMContext):
-    current_data = await state.get_data()
+    configuration = await state.get_data()
     await message.answer(
-        "\n".join(f"{key}: {current_data[key]}" for key in current_data)
-        if current_data else "Connect button will appear after all settings have been added...",
-        reply_markup=generate_connection_keyboard(**current_data)
+        "\n".join(f"{option}: {value}" for option, value in configuration.items())
+        if configuration else "Connect button will appear after all settings have been added...",
+        reply_markup=generate_connection_keyboard(**configuration)
     )
-
-
-async def cmd_connection(message: types.Message, state: FSMContext):
-    # if await state.get_state():
-    #     pass
-    await generate_connection_menu(message, state)
+    await ConnectionStatus.configuration.set()
 
 
 async def configure_connection_button(callback: types.CallbackQuery, state: FSMContext):
     await generate_connection_menu(callback.message, state)
     await callback.answer()
+
+
+async def option_button(callback: types.CallbackQuery, state: FSMContext):
+    """Universal function of the invitation to input SSH configuration option corresponding to the button."""
+    option = callback.data.split('_')[0]
+    await callback.message.edit_text(f"Enter the {option}.")
+    await state.set_state(f"ConfigurationOptions:{option}")
+
+
+async def enter_option_value(message: types.Message, state: FSMContext):
+    """Universal function for entering SSH configuration option value corresponding to the state."""
+    current_state = await state.get_state()
+    option = current_state.split(':')[1]
+    await update_option_value(option, message, state)
+
+
+async def update_option_value(option: str, message: types.Message, state: FSMContext) -> None:
+    if len(message.text) == 4096:
+        await message.reply(f"Please note that this message will be added as {option} value.")
+    configuration = await state.get_data()
+    configuration[option] = message.text
+    await state.update_data(configuration)
+    await generate_connection_menu(message, state)
 
 
 async def undefined_request(message: types.Message):
@@ -44,6 +74,8 @@ async def undefined_request(message: types.Message):
 
 def register_handlers(dp: Dispatcher):
     dp.register_message_handler(cmd_start, IsAdmin(), CommandStart(), state="*")
-    dp.register_message_handler(cmd_connection, IsAdmin(), commands=["connection"], state="*")
-    dp.register_callback_query_handler(configure_connection_button, Text(equals="configure_connection"))
+    dp.register_message_handler(generate_connection_menu, IsAdmin(), commands=["connection"], state="*")
+    dp.register_callback_query_handler(configure_connection_button, Text(equals="connection"))
+    dp.register_callback_query_handler(option_button, Text(endswith="option"), state=ConnectionStatus.configuration)
+    dp.register_message_handler(enter_option_value, state=ConfigurationOptions.states_names)
     dp.register_message_handler(undefined_request, content_types=ContentType.ANY, state="*")
--- bot/keyboards.py
+++ bot/keyboards.py
@@ -2,21 +2,23 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 def generate_connection_keyboard(
-        host: str = None, username: str = None, password: str = None, port: str = None
+        hostname: str = None, user: str = None, password: str = None, port: str = None
 ) -> InlineKeyboardMarkup:
     connection_keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(f"{'✔' if host else '➕'} Host", callback_data="host_field")],
-            [InlineKeyboardButton(f"{'✔' if username else '➕'} Username", callback_data="username_field")],
-            [InlineKeyboardButton(f"{'✔' if password else '➕'} Password", callback_data="password_field")],
-            [InlineKeyboardButton(f"{'✔' if port else '➕'} Port", callback_data="port_field")]
+            [InlineKeyboardButton(f"{'✔' if hostname else '➕'} HostName", callback_data="hostname_option")],
+            [InlineKeyboardButton(f"{'✔' if port else '➕'} Port", callback_data="port_option")],
+            [InlineKeyboardButton(f"{'✔' if user else '➕'} User", callback_data="user_option")],
+            [InlineKeyboardButton(f"{'✔' if password else '➕'} Password", callback_data="password_option")]
         ]
     )
+    if hostname and user and password and port:
+        connection_keyboard.add(InlineKeyboardButton("👨🏻‍💻 Connect", callback_data="connect"))
     return connection_keyboard
 
 
 start_menu_keyboard = InlineKeyboardMarkup(
     inline_keyboard=[
-        [InlineKeyboardButton("⚙ Connect via SSH", callback_data="configure_connection")]
+        [InlineKeyboardButton("⚙ Connect via SSH", callback_data="connection")]
     ]
 )
